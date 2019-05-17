d14cc3b40c2a441e72c07ff8a271a28eb119e8ca
719224032@qq.com <11171992zhu>
2019-05-17 08:44:03 +0800

    接入logback

--- rat-admin/src/main/java/com/jason/rat/admin/RatAdminApplication.java
+++ rat-admin/src/main/java/com/jason/rat/admin/RatAdminApplication.java
@@ -1,13 +1,13 @@
-package com.jason.rat.ratclient;
+package com.jason.rat.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RatClientApplication {
+public class RatAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RatClientApplication.class, args);
+        SpringApplication.run(RatAdminApplication.class, args);
     }
 
 }
--- rat-portal/src/main/java/com/jason/rat/portal/controller/UserController.java
+++ rat-portal/src/main/java/com/jason/rat/portal/controller/UserController.java
@@ -3,6 +3,8 @@ package com.jason.rat.portal.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.jason.rat.client.model.User;
 import com.jason.rat.client.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
     @Reference(version = "1.0.0")
     private UserService userService;
 
     @GetMapping("/userInfo/{id}")
     public User getUserInfo(@PathVariable Long id) {
-        return userService.findById(id);
+        try {
+            return userService.findById(id);
+        } catch (Exception e) {
+            logger.error("userService.findById error,id=" + id, e);
+        }
+        return null;
     }
 }
