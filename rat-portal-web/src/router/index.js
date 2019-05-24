081d93dde015fe5f143636e92d75a2b16c1d7b42
719224032@qq.com <11171992zhu>
2019-05-25 01:42:09 +0800

    add portal web page

--- rat-portal-web/src/router/index.js
+++ rat-portal-web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/manage/Login.vue'
+import Index from '@/components/home/Index.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
+    },
+    {
+      path: '/index',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/manage',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
